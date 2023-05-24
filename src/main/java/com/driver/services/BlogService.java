5fca556dfd5b52364efda67030cc8519efb04152
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-05-24 08:27:54 +0000

    Changes done

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -25,20 +25,36 @@ public class BlogService {
     public Blog createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
 
-        Optional<User> userOpt = userRepository1.findById(userId);
-        User user = userOpt.get();
+        // Optional<User> userOpt = userRepository1.findById(userId);
+        // User user = userOpt.get();
+
+        // Blog blog = new Blog(title, content);
+        // blog.setUser(user);
+
+        // List<Blog> bloglList = user.getBlogList();
+
+        // bloglList.add(blog);
+        // user.setBlogList(bloglList);
+
+        // User savedUser = userRepository1.save(user);
+
+        // Blog savedBlog = savedUser.getBlogList().get(savedUser.getBlogList().size()-1);
+        // return savedBlog;
+
+        Optional<User> optionalUser = userRepository1.findById(userId);
+        User user = optionalUser.get();
 
         Blog blog = new Blog(title, content);
         blog.setUser(user);
 
-        List<Blog> bloglList = user.getBlogList();
-
-        bloglList.add(blog);
-        user.setBlogList(bloglList);
+        List<Blog> blogList= user.getBlogList();
+        blogList.add(blog);
+        user.setBlogList(blogList);
 
         User savedUser = userRepository1.save(user);
 
-        Blog savedBlog = savedUser.getBlogList().get(savedUser.getBlogList().size()-1);
+        Blog savedBlog = savedUser.getBlogList().get(savedUser.getBlogList().size() -1);
+
         return savedBlog;
     }
 
