d630aea5bc238319be9cc63112a3e6c4cbffd29c
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-05-24 09:29:13 +0000

    dsndnb

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -1,38 +1,3 @@
-// package com.driver.controller;
-
-// import com.driver.models.Blog;
-// import com.driver.services.BlogService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/blogs")
-// public class BlogController {
-
-//     @Autowired
-//     BlogService blogService;
-
-//     @PostMapping
-//     public ResponseEntity createBlog(@RequestParam Integer userId ,
-//                                      @RequestParam String title,
-//                                      @RequestParam String content) {
-//         // Create a blog and add it under given user
-//         Blog blog = blogService.createAndReturnBlog(userId, title, content);
-//         return new ResponseEntity<>(HttpStatus.CREATED);
-//     }
-
-//     @DeleteMapping("/{blogId}")
-//     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
-//         // Delete the blog using deleteById
-//         blogService.deleteBlog(blogId);
-//         return new ResponseEntity<>(HttpStatus.OK);
-//     }
-// }
-
 package com.driver.controller;
 
 import com.driver.models.Blog;
@@ -54,14 +19,14 @@ public class BlogController {
     @PostMapping("/createBlog")
     public ResponseEntity createBlog(@RequestParam Integer userId , @RequestParam String title, @RequestParam String content) {
         // Create a blog and add it under given user
-            Blog savedBlog = blogService.createAndReturnBlog(userId,title,content);
+        Blog blog = blogService.createAndReturnBlog(userId,title,content);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
     public ResponseEntity<Void> deleteBlog(@PathVariable int blogId) {
         // Delete the blog using deleteById
-            blogService.deleteBlog(blogId);
+        blogService.deleteBlog(blogId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,62 +1,3 @@
-// package com.driver.services;
-
-// import com.driver.models.*;
-// import com.driver.repositories.*;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-
-// import java.util.List;
-// import java.util.Optional;
-
-// @Service
-// public class ImageService {
-
-//     @Autowired
-//     BlogRepository blogRepository2;
-//     @Autowired
-//     ImageRepository imageRepository2;
-
-//     public Image addImage(Integer blogId, String description, String dimensions){
-//         //add an image to the blog
-//         Optional<Blog> blogOpt = blogRepository2.findById(blogId);
-//         Blog blog = blogOpt.get();
-
-//         Image image = new Image(description, dimensions);
-//         image.setBlog(blog);
-
-//         List<Image> imageList = blog.getImageList();
-
-//         imageList.add(image); // Update the image list
-//         Blog savedBlog = blogRepository2.save(blog);
-//         return image;
-//     }
-
-//     public void deleteImage(Integer id){
-//         imageRepository2.deleteById(id);
-//     }
-
-//     public int countImagesInScreen(Integer id, String screenDimensions) {
-//         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-//         Optional<Image> imageOpt = imageRepository2.findById(id);
-//         Image image = imageOpt.get();
-
-//         /*Image dimension --> 2*2 and screen dimension --> 4*4 */
-//         String imagedimension = image.getDimensions();
-
-//         String [] imageArr = imagedimension.split("X");
-
-//         String [] screenArr = screenDimensions.split("X");
-
-//         int screenArea = Integer.parseInt(screenArr[0]) * Integer.parseInt(screenArr[1]);
-//         int imageArea = Integer.parseInt(imageArr[0]) * Integer.parseInt(imageArr[1]);
-
-//         return screenArea/imageArea;
-
-//     }
-// }
-
-
-
 package com.driver.services;
 
 import com.driver.models.*;
@@ -86,62 +27,60 @@ public class ImageService {
         image.setBlog(blog);
 
         List<Image> imageList = blog.getImageList();
-        imageList.add(image);
+        imageList.add(image); // Update the image list
         blogRepository2.save(blog);
 
-
         return image;
-
     }
 
     public void deleteImage(Integer id){
-
-//        Optional<Image> optionalImage = imageRepository2.findById(id);
-//
-//        Image image = optionalImage.get();
-//
-//        Blog blog = image.getBlog();
-//
-//        List<Image> imageList = blog.getImageList();
-//
-//        imageList.remove(image);
-//
-//       // imageRepository2.deleteById(id);
-
         imageRepository2.deleteById(id);
-
-
     }
 
 
     public int countImagesInScreen(Integer imageId, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
 
-        Optional<Image> optionalImage = imageRepository2.findById(imageId);
+        // Optional<Image> optionalImage = imageRepository2.findById(imageId);
 
-        Image image = optionalImage.get();
+        // Image image = optionalImage.get();
 
-        String dimension = image.getDimensions();
+        // String dimension = image.getDimensions();
 
-        // change the screen dimension to the Integers value
-        String str[] = screenDimensions.split("X");
+        // // change the screen dimension to the Integers value
+        // String str[] = screenDimensions.split("X");
 
-        int heightScreen = Integer.valueOf(str[0]);
-        int widthScreen = Integer.valueOf(str[1]);
+        // int heightScreen = Integer.valueOf(str[0]);
+        // int widthScreen = Integer.valueOf(str[1]);
 
-        // change the current Image dimension to the Integers value
-        String strImage[] = dimension.split("X");
+        // // change the current Image dimension to the Integers value
+        // String strImage[] = dimension.split("X");
 
-        int heightImage = Integer.valueOf(strImage[0]);
-        int widthImage = Integer.valueOf(strImage[1]);
-
-
-        int heightRatio = heightScreen/heightImage;
-        int widthRatio = widthScreen/widthImage;
+        // int heightImage = Integer.valueOf(strImage[0]);
+        // int widthImage = Integer.valueOf(strImage[1]);
 
 
-        int count = heightRatio * widthRatio;
+        // int heightRatio = heightScreen/heightImage;
+        // int widthRatio = widthScreen/widthImage;
 
-        return count;
+
+        // int count = heightRatio * widthRatio;
+
+        // return count;
+
+        Optional<Image> imageOpt = imageRepository2.findById(id);
+        Image image = imageOpt.get();
+        
+        /*Image dimension --> 2*2 and screen dimension --> 4*4 */
+        String imagedimension = image.getDimensions();
+
+        String [] imageArr = imagedimension.split("X");
+
+        String [] screenArr = screenDimensions.split("X");
+
+        int screenArea = Integer.parseInt(screenArr[0]) * Integer.parseInt(screenArr[1]);
+        int imageArea = Integer.parseInt(imageArr[0]) * Integer.parseInt(imageArr[1]);
+
+        return screenArea/imageArea;
     }
 }
